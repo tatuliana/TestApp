aa09b79bf7194480e1a01b0bfe7087798b0971b3
Tatuliana <nataliolg@gmail.com>
2024-11-23 22:35:24 -0800

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Natalia Popova on 11/23/24.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
